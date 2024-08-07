1200fc4f908324fe03b03da766811b0886e66c41
Andrew Park <136746782+andrewpark0408@users.noreply.github.com>
2024-08-07 16:17:31 -0700

    add routing for pages and create bank card for right side bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Andrew' }
+  const loggedIn = { firstName: 'Andrew', lastName: 'Park', email: 'andrew@email.com' }
 
   return (
     <section className="home">
@@ -25,6 +26,11 @@ const Home = () => {
 
         RECENT TRANSCATIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]}
+      />
     </section>
   )
 }
